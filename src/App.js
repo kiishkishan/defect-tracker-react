21f35740db488e5af5b3dfa37288868b7947ec07
DalistaaA <adalistaa@gmail.com>
2019-03-26 16:16:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import Login from './Components/Login';
+// import Login from "./Components/Login";
+import AddModule from "./Components/AddModule";
 // import AddDefectForm from './Components/AddDefectForm';
-
 
 class App extends Component {
   render() {
     return (
-      <div className="card-image">
+      <div>
         <MDBContainer>
-          <MDBRow className="row-height"></MDBRow>
+          <MDBRow className="row-height" />
           <MDBRow>
-            <MDBCol md="4"></MDBCol>
-            <MDBCol md="4"><Login /></MDBCol>
-            <MDBCol md="4"></MDBCol>
+            <MDBCol md="4" />
+            <MDBCol md="4">
+              <AddModule />
+            </MDBCol>
+            <MDBCol md="4" />
           </MDBRow>
         </MDBContainer>
-      </div >
-
+      </div>
     );
   }
 }
--- src/Components/AddModule.jsx
+++ src/Components/AddModule.jsx
@@ -1,14 +1,58 @@
-import React, { Component } from 'react';
-import { MDBContainer } from 'mdbreact';
+import React from "react";
+import { MDBInput, MDBBtn, MDBCard, MDBCardBody } from "mdbreact";
 
-class AddModule extends Component {
-    render() {
-        return (
-            <MDBContainer>
-
-            </MDBContainer>
-        );
-    }
-}
+const AddModule = () => {
+  return (
+    <MDBCard>
+      <MDBCardBody>
+        <form>
+          <div className="image-align">
+            <i class="prefix grey-text">
+              <img
+                src="https://img.icons8.com/color/65/000000/module.png"
+                alt="asdf"
+              />
+            </i>
+            <p className="h4 mb-4">Modules</p>
+          </div>
+          <br />
+          <div className="grey-text">
+            <MDBInput
+              label="Module ID"
+              icon="key"
+              group
+              type="text"
+              validate
+              error="wrong"
+              success="right"
+            />
+            <MDBInput
+              label="Module Name"
+              icon="puzzle-piece"
+              group
+              type="text"
+              validate
+              error="wrong"
+              success="right"
+            />
+          </div>
+          <div className="text-center py-4 mt-3">
+            <MDBBtn
+              type="button"
+              gradient="blue"
+              rounded
+              className="btn-block z-depth-1a"
+            >
+              Add
+            </MDBBtn>
+            {/* <MDBBtn color="cyan" type="submit">
+                            Register
+                  </MDBBtn> */}
+          </div>
+        </form>
+      </MDBCardBody>
+    </MDBCard>
+  );
+};
 
 export default AddModule;
